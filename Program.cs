3a9658265cedef3d89a235bae140d1b92e11ce27
Dion4286 <101573639+Dion4286@users.noreply.github.com>
2022-04-07 19:52:00 +0700

    Latihan Overloading Method
    Nama: Dionisius Rizky Pramusinto
    NIM: 21.11.4286
    Kelas: 21-IF-07

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace LatihanOverloadingMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers number = new Numbers();
+            Console.WriteLine("Minimum #1: {0}", number.FindMinimum(3, 1));
+            Console.WriteLine("Minimum #2: {0}", number.FindMinimum(3, 2, 4));
+            Console.WriteLine();
+
+            Console.WriteLine("Maximum #1: {0}", number.FindMaximum(3, 1));
+            Console.WriteLine("Maximum #2: {0}", number.FindMaximum(3, 2, 4));
+
+            Console.ReadKey();
+        }
+    }
+}
